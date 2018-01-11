e6d3f6bf9caaa1fd222e41e27fa0cff2d359cb8f
Tobias Lenz <tobias@qfs.de>
2018-01-11 18:20:44 +0100

    Added stubs for FullScreen

---
+++ src/main/java/com/apple/eawt/FullScreenUtilities.java
@@ -0,0 +1,12 @@
+package com.apple.eawt;
+
+import java.awt.Window;
+
+public class FullScreenUtilities {
+
+    public FullScreenUtilities() {}
+    public static void setWindowCanFullScreen(Window window, boolean canFullScreen) {}
+    public static void addFullScreenListenerTo(Window window, FullScreenListener listener) {}
+    public static void removeFullScreenListenerFrom(Window window, FullScreenListener listener) {}
+
+}
